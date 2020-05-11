807caa7e3387e37a12b3c7bb40af5a669067c591
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-11 06:04:45 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,7 @@ class Anagram
   end
   
   def match(array)
-    array.keep_if { |str| name.split('').sort == str.split('') }
+    array.keep_if { |str| name.split('').sort == str.split('').sort }
   end
   
 end
