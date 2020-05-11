d5d4eb9803ee1704c9c212b180bce4eeb87b56e2
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-11 06:04:59 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+require "pry"
+class Anagram
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+  
+  def match(array)
+    array.keep_if { |str| name.split('').sort == str.split('').sort }
+  end
+  
+end
